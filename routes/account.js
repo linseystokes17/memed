dbc12d6b4cf9d447428dcf32864c37a52d2d3433
Linsey Stokes <linseystokes17@gmail.com>
2020-12-16 16:16:15 -0700

    updates

--- routes/account.js
+++ routes/account.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('account'{
-    user: req.user
-  });
+  res.render('account');
 });
 
 module.exports = router;
